539b8261d251b7445fe08452967d3a793f989640
Ericson Palisoc <palisocericson87@gmail.com>
2023-05-06 01:45:43 +0800

    change font and add some feature

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const body = document.body;
-const moon_icon = document.querySelector('.ri-moon-clear-line');
+const moon_icon = document.querySelector('.bx-moon');
 const menu_icon = document.querySelector('.bx-menu-alt-right');
 const navlist = document.querySelector('.navlist');
 const nav = document.querySelector('header');
@@ -7,12 +7,12 @@ const expand_btn = document.querySelector('.expand-btn');
 const hidden = document.querySelectorAll('.hide');
 
 moon_icon.addEventListener('click', () => {
-    if(moon_icon.classList.contains('ri-moon-clear-line')) {
-        moon_icon.classList.remove('ri-moon-clear-line');
+    if(moon_icon.classList.contains('bx-moon')) {
+        moon_icon.classList.remove('bx-moon');
         moon_icon.classList.add('bx-sun');
     } else {
         moon_icon.classList.remove('bx-sun');
-        moon_icon.classList.add('ri-moon-clear-line');
+        moon_icon.classList.add('bx-moon');
     }
 
     body.classList.toggle('dark');
@@ -56,15 +56,15 @@ function sampleFunc() {
 
 sampleFunc();
 
-// const sr = ScrollReveal ({
-//     distance: '30px',
-//     duration: 1500,
-//     reset: true
-// })
+const sr = ScrollReveal ({
+    distance: '30px',
+    duration: 1500,
+    reset: true
+})
 
-// sr.reveal('.home', {delay: 80, origin: 'left'});
-// sr.reveal('.lyrics', {delay: 80, origin: 'right'});
-// sr.reveal('.contact-us', {delay: 80, origin: 'bottom'});
+sr.reveal('.home', {delay: 80, origin: 'left'});
+sr.reveal('.lyrics', {delay: 80, origin: 'right'});
+sr.reveal('.contact-us', {delay: 80, origin: 'bottom'});
 
 // const boxes = document.querySelectorAll('.box');
 // const overlays = document.querySelector('.overlay');
@@ -84,65 +84,10 @@ sampleFunc();
 // }
 
 // hoverBoxes();
-const openBox = document.querySelectorAll('.vid-container');
-const playBtn = document.querySelectorAll('.play-btn');
-const overlay = document.querySelectorAll('.overlay');
-const smapleExit = document.querySelectorAll('.sampleBtn');
+const playBtn = document.querySelector('.play-btn');
+playBtn.addEventListener('click', () => {
 
-const pola = document.querySelectorAll('.theVid');
-for(let i=0; i<playBtn.length; i++) {
-    playBtn[i].addEventListener('click', () => {
-
-        openBox[i].classList.toggle('open-vid');
-        overlay[i].classList.toggle('remove-overlay');
-    
-        pola[i].play();
-    })
-}
-
-for(let i=0; i<smapleExit.length; i++) {
-    smapleExit[i].addEventListener('click', function() {
-        if(openBox[i].classList.contains('open-vid')) {
-
-            openBox[i].classList.remove('open-vid');
-            overlay[i].classList.remove('remove-overlay');
-    
-            pola[i].pause();
-            pola[i].currentTime = 0;
-        }
-    })
-}
-
-
-TweenMax.from('.navlist, .logo', 1, {
-    opacity: 0,
-    y: -20,
-    ease: Expo.easeInOut
 })
-
-TweenMax.from('.moon-icon, .menu-icon', 1, {
-    opacity: 0,
-    x: 20,
-    ease: Expo.easeInOut
-})
-
-TweenMax.from('h1, h3, #impor', 2, {
-    opacity: 0,
-    y: -50,
-    ease: Power3.easeInOut
-})
-
-TweenMax.from('button', 1, {
-    opacity: 0,
-    y: 10,
-    ease: Expo.easeInOut
-})
-
-
-
-
-
-
 
 
 
